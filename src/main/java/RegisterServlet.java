fedbbac03a5562fbf3e700d40960e84df2ea5db5
bhavesh357 <bkadam357@gmail.com>
2020-06-04 23:44:01 +0530

    ADDED: password validation

--- src/main/java/RegisterServlet.java
+++ src/main/java/RegisterServlet.java
@@ -9,22 +9,28 @@ import java.io.PrintWriter;
 
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
-    String namePattern="^[A-Z][a-z]{2,}$";
+    private String namePattern="^[A-Z][a-z]{2,}$";
+    private String passwordPattern="^(?=.*[A-Z])(?=.*[0-9])(?=.*[A-Za-z0-9@#!$%^&*()_-]{8,})[A-Za-z0-9]+?[@#!$%^&*()_-][A-Za-z0-9]{1,}?$";
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String user = request.getParameter("user");
         String password = request.getParameter("password");
         String passwordRepeat = request.getParameter("passwordRepeat");
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/register.jsp");
+        PrintWriter writer = response.getWriter();
 
-        if(password.equals(passwordRepeat) && user.matches(namePattern)){
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/register.jsp");
-            PrintWriter writer = response.getWriter();
-            writer.println("<font color=green>Successfully Registered</font>");
-            rd.include(request,response);
-        }else{
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/register.jsp");
-            PrintWriter writer = response.getWriter();
-            writer.println("<font color=red>Either username is not valid or passwords dont match</font>");
-            rd.include(request,response);
+        if(password.equals(passwordRepeat)){
+            if(user.matches(namePattern)){
+                if (password.matches(passwordPattern)){
+                    writer.println("<font color=green>Successfully Registered</font>");
+                }else {
+                    writer.println("<font color=red>Password is not valid</font>");
+                }
+            }else{
+                writer.println("<font color=red>Username is not valid</font>");
+            }
+        }else {
+            writer.println("<font color=red>password dont match</font>");
         }
+        rd.include(request,response);
     }
 }
